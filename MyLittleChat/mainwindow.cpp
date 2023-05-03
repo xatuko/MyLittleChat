a219b8ad5363e5860b959b64d4bafc6f642904f4
Ivan Shmelev <vin.ven@yandex.ru>
2023-05-03 13:10:53 +0300

    fix bug when api key reading from file with \n at end

--- MyLittleChat/mainwindow.cpp
+++ MyLittleChat/mainwindow.cpp
@@ -12,14 +12,14 @@ MainWindow::MainWindow(QWidget *parent)
 
     request.setUrl(QUrl("https://api.openai.com/v1/chat/completions"));
     request.setRawHeader(QByteArray("Content-Type"), QByteArray("application/json"));
-
     QFile file("api_key");
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         qDebug() << "file not opened";
         return;
     }
-    QString key_header = "Bearer " + QString::fromStdString(file.readLine().toStdString());
+    QString key_header = "Bearer " + file.readAll();
+    key_header = key_header.trimmed();
     request.setRawHeader(QByteArray("Authorization"),
                          QByteArray(qPrintable(key_header)));
     file.close();
@@ -51,8 +51,5 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::replyFinished()
 {
     ui->textEdit_2->clear();
-    QByteArray response = reply->readAll();
-    QString resp = QJsonDocument::fromJson(response).object()["choices"][0]["message"]["content"].toString();
-    ui->textEdit_2->setText(resp);
+    ui->textEdit_2->setText(QJsonDocument::fromJson(reply->readAll()).object()["choices"].toArray()[0].toObject()["message"].toObject()["content"].toString());
 }
-
